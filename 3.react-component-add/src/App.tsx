510edf7b994405fa87117ca8283cc9b9f011b2ea
wujiahui <wujiahui.grap@bytedance.com>
2023-11-26 23:22:17 +0800

    feat: 自增

--- 3.react-component-add/src/App.tsx
+++ 3.react-component-add/src/App.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import './index.css';
+import React, { useCallback, useEffect, useRef, useState } from "react";
+import "./index.css";
 
 async function increaseRemote(a: number) {
   await new Promise((resolve) => setTimeout(resolve, Math.random() * 1e3));
@@ -12,15 +12,55 @@ async function increaseRemote(a: number) {
  * 2. 由于是异步相加，自增过程中需要将 button 置为 disabled 状态，不可响应点击；
  */
 function App() {
+  const [count, setCount] = useState(0);
+  const refCount = useRef(0);
+  const loading = useRef(false);
+  const increase = useCallback(async () => {
+    if (loading.current) {
+      return;
+    }
+    loading.current = true;
+    const data = await increaseRemote(refCount.current);
+    setCount(data);
+    refCount.current = data;
+    loading.current = false;
+  }, [count]);
+
+  const handleClick = async (num: 1 | 2) => {
+    if (num === 1) {
+      increase();
+    } else if (num === 2) {
+      await increase();
+      increase();
+    }
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <div>加法计数器</div>
       </header>
       <section className="App-content">
-        <button>+1</button>
-        <button>+2</button>
-        <p>数值：{'计算结果'}</p>
+        <button
+          disabled={loading.current}
+          onClick={() => {
+            handleClick(1);
+          }}
+        >
+          +1
+        </button>
+        <button
+          disabled={loading.current}
+          onClick={() => {
+            handleClick(2);
+          }}
+        >
+          +2
+        </button>
+        <p>
+          数值：{count}
+          {"计算结果"}
+        </p>
       </section>
     </div>
   );
